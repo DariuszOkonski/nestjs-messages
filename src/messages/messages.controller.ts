2c3f33c11f3770e6e92df4da93568baef19a964d
Dariusz Okoński <darek200180@gmail.com>
2025-12-03 20:16:56 +0100

    add service to controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,28 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // dont do that, use dependency injection
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    return [];
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log('body: ', body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log('id: ', id);
-    return [];
+    return this.messagesService.findOne(id);
   }
 }
